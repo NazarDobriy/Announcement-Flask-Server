7187a422bcf0ae9b41e68bac172916b2b8708034
NazarDobriy <nazaranddobriy@gmail.com>
2022-04-13 23:10:00 +0300

    fix

--- main.py
+++ main.py
@@ -1,129 +1,114 @@
-import os
-
+import sqlite3
 from flask import Flask, request, make_response, jsonify, abort
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_cors import CORS
+from sqlalchemy.engine import row
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///announcement.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-SQLALCHEMY_DB_URL = os.getenv("DB_CONN")
 
 
-class Announcement(db.Model):
-
-    def __init__(self, title=None, description=None):
-        self.title = title
-        self.description = description
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
-
-class AnnouncementController(object):
-
-    def __init__(self, model_announcement=Announcement()):
-        self.model_announcement = model_announcement
-
-    def create(self, announcement_data=None):
-        self.model_announcement.title = announcement_data.get('title')
-        self.model_announcement.description = announcement_data.get('description')
-
-        if not self.model_announcement.title:
-            abort(400, "You miss title")
-
-        elif not self.model_announcement.description:
-            abort(400, "You miss description")
-
-        else:
-            announcement = Announcement(title=self.model_announcement.title, description=self.model_announcement.description)
-            db.session.add(announcement)
-            db.session.commit()
-            return make_response(jsonify({"message": "An announcement was created"}, 200))
-
-    def get_all_announcements(self):
-        announcements = Announcement.query.order_by(Announcement.date.desc()).all()
-        announcements_arr = []
-
-        for announcement in announcements:
-            announcements_arr.append({
-                "id": announcement.id,
-                "title": announcement.title,
-                "description": announcement.description,
-                "date": announcement.date.strftime('%d.%m.%Y')
-            })
-
-        return jsonify(announcements_arr)
-
-    def get_announcement_by_id(self, current_id=None):
-        if current_id:
-            announcement = Announcement.query.get(current_id)
-
-            if not announcement:
-                abort(404, "Announcement with this id isn't found")
-
-            announcement_json = {
-                "title": announcement.title,
-                "description": announcement.description,
-                "date": announcement.date.strftime('%d.%m.%y')
-            }
-            return announcement_json
-        abort(404)
-
-    def update(self, current_id=None):
-        if current_id:
-            announcement = Announcement.query.get(current_id)
-            new_announcement_data = request.get_json()
-
-            if new_announcement_data.get('title'):
-                announcement.title = new_announcement_data.get('title')
-
-            if new_announcement_data.get('description'):
-                announcement.description = new_announcement_data.get('description')
-
-            db.session.commit()
-
-            return make_response(jsonify({"message": "An announcement was updated"}, 200))
-        abort(404, "Announcement with this id is not found")
-
-    def delete(self, current_id=None):
-        if current_id:
-            selected_announcement = Announcement.query.get(current_id)
-            db.session.delete(selected_announcement)
-            db.session.commit()
-            return jsonify({"message": "Announcement was deleted"}, 200)
-        abort(404, "Announcement with this id is not found")
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('announcements.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 
 @app.route('/create-announcement', methods=['POST'])
 def create_announcement():
+    conn = db_connection()
     announcement_data = request.get_json()
-    return AnnouncementController().create(announcement_data)
+
+    new_title = announcement_data.get('title')
+    new_description = announcement_data.get('description')
+
+    sql = """ INSERT INTO announcement (title, description) VALUES (?, ?) """
+
+    conn.execute(sql, (new_title, new_description))
+    conn.commit()
+
+    return jsonify({"message": "An announcement was created"}, 200)
 
 
 @app.route('/all-announcements', methods=['GET'])
 def all_announcements():
-    return AnnouncementController().get_all_announcements()
+    announcements = None
+
+    conn = db_connection()
+    cursor = conn.execute("SELECT * FROM announcement")
+
+    announcements = [
+        dict(id=row[0], title=row[1], description=row[2], date=row[3])
+        for row in cursor.fetchall()
+    ]
+
+    if announcements is not None:
+        return jsonify(announcements)
 
 
 @app.route('/announcement/<int:id>', methods=['GET'])
 def announcement(id):
-    return AnnouncementController().get_announcement_by_id(id)
+    announcement = {}
+
+    conn = db_connection()
+    cursor = conn.execute("SELECT * FROM announcement WHERE id == ?", (id,))
+
+    rows = cursor.fetchall()
+
+    for r in rows:
+        announcement['title'] = r[1]
+        announcement['description'] = r[2]
+        announcement['date'] = r[3]
+
+    if announcement is not None:
+        return jsonify(announcement)
 
 
 @app.route('/update-announcement/<int:id>', methods=['PUT'])
 def update_announcement(id):
-    return AnnouncementController().update(id)
+    conn = db_connection()
+
+    sql = """ UPDATE announcement
+        SET title = ?,
+        description = ?
+        WHERE id = ?
+     """
+
+    announcement = {}
+
+    new_announcement_data = request.get_json()
+
+    if new_announcement_data.get('title'):
+        announcement["title"] = new_announcement_data.get('title')
+
+    if new_announcement_data.get('description'):
+        announcement["description"] = new_announcement_data.get('description')
+
+    updated_announcement = {
+        "id": id,
+        "title": announcement["title"],
+        "description": announcement["description"]
+    }
+
+    conn.execute(sql, (announcement["title"], announcement["description"], id))
+    conn.commit()
+
+    return jsonify(updated_announcement)
 
 
 @app.route('/delete-announcement/<int:id>', methods=['DELETE'])
 def delete_announcement(id):
-    return AnnouncementController().delete(id)
+    conn = db_connection()
+
+    sql = """ DELETE FROM announcement WHERE id = ? """
+
+    conn.execute(sql, (id,))
+    conn.commit()
+
+    return jsonify({"message": "Announcement was deleted"}, 200)
 
 
 if __name__ == "__main__":
